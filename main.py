0ed0026bf35b1166b8defe6ad93c02eb2abb362a
Lucas Moraes <83236822+luca-moraes@users.noreply.github.com>
2023-09-19 22:58:27 +0000

    updATE

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ arquivo = np.load('teste1.npy')
 x = arquivo[0]
 y = np.ravel(arquivo[1])
 
-
-
-
-
 regr = MLPRegressor(hidden_layer_sizes=(2),
                     max_iter=100,
                     activation='relu', #{'identity', 'logistic', 'tanh', 'relu'},
@@ -20,15 +16,8 @@ regr = MLPRegressor(hidden_layer_sizes=(2),
 print('Treinando RNA')
 regr = regr.fit(x,y)
 
-
-
 print('Preditor')
 y_est = regr.predict(x)
-
-
-
-
-
 
 plt.figure(figsize=[14,7])
 
@@ -44,8 +33,5 @@ plt.plot(regr.loss_curve_)
 plt.subplot(1,3,3)
 plt.plot(x,y,linewidth=1,color='yellow')
 plt.plot(x,y_est,linewidth=2)
-
-
-
 
 plt.show()
