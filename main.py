5f419055760dea60690a9bd911aa6bc2b1a7c7b6
luca-moraes <lcansd.lm@gmail.com>
2023-09-26 18:09:42 -0300

    sim 3

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ regr5_sim1 = MLPRegressor(hidden_layer_sizes=(100,80,70,40,20,10),max_iter=52200
 
 regr5_sim2 = MLPRegressor(hidden_layer_sizes=(60,70,50,20),max_iter=322000,activation='relu',solver='adam',learning_rate='adaptive',n_iter_no_change=5000)
 
-regr5_sim3 = MLPRegressor(hidden_layer_sizes=(80,40),max_iter=82000,activation='relu',solver='adam',learning_rate='adaptive',n_iter_no_change=1400)
+regr5_sim3 = MLPRegressor(hidden_layer_sizes=(120,60),max_iter=322000,activation='relu',solver='adam',learning_rate='adaptive',n_iter_no_change=22000)
 
-regr = regr5_sim1
+regr = regr5_sim3
 
 print('Carregando Arquivo de teste')
 
@@ -78,7 +78,7 @@ for i in range(1):
     plt.plot(x,y_est,linewidth=2)
 
     #plt.show()
-    plt.savefig(f'./imagens/teste5/sim1/E{i+1}.png')
+    plt.savefig(f'./imagens/teste5/sim3/E{i+1}.png')
 
 media = media / 10
 
